621c120fffd1920f4f1efe85e40550e8c99bfebf
Bhavitha <bhavitagottimukkula@gmail.com>
2024-08-06 18:01:03 +0530

    Initial Commit

---
+++ DataFetchUITests/DataFetchUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DataFetchUITestsLaunchTests.swift
+//  DataFetchUITests
+//
+//  Created by Bhavitha Gottimukkula on 06/08/24.
+//
+
+import XCTest
+
+final class DataFetchUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
